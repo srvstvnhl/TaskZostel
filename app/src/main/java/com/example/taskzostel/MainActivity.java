2c18311e3c3250b53858bc05e65f54f364120180
srvstvnhl <srvstvnhl@gmail.com>
2020-10-27 13:29:07 +0530

    Sent token to server

--- app/src/main/java/com/example/taskzostel/MainActivity.java
+++ app/src/main/java/com/example/taskzostel/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.taskzostel;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +23,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.Objects;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,23 +42,21 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(input);
 
-        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.O){
-            NotificationChannel notificationChannel = new NotificationChannel("MyNotification","Notification", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager  manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(notificationChannel);
-        }
-//        FirebaseApp.initializeApp(this);
-        FirebaseMessaging.getInstance().subscribeToTopic("general")
-                .addOnCompleteListener(task -> {
-                    String msg ="Successful";
-                    if (!task.isSuccessful()) {
-                        msg = "Failed";
-                    }
-                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                });
+//        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.O){
+//            NotificationChannel notificationChannel = new NotificationChannel("MyNotification","Notification", NotificationManager.IMPORTANCE_DEFAULT);
+//            NotificationManager  manager = getSystemService(NotificationManager.class);
+//            manager.createNotificationChannel(notificationChannel);
+//        }
+////        FirebaseApp.initializeApp(this);
+//        FirebaseMessaging.getInstance().subscribeToTopic("general")
+//                .addOnCompleteListener(task -> {
+//                    String msg ="Successful";
+//                    if (!task.isSuccessful()) {
+//                        msg = "Failed";
+//                    }
+//                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                });
 
     }
-
-
 
 }
